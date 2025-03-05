1221f6a2f70f154e35870e0970adc28210b9fece
Lawrence Paul Cado Marino <lawrencemarino18@gmail.com>
2025-03-05 14:56:12 +0800

    Added Ingredient, Recipe, and RecipeIngredient models

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("ledger:ingredient-detail", args=[str(self.id)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("ledger:recipe-detail", args=[str(self.id)])
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=100)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name} for {self.recipe.name}"
